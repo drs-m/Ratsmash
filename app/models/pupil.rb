98839e32c7182764e182ee68b11eb9d58bcdc2d2
Darius Mewes <dariusmewes@me.com>
2014-03-25 00:04:56 +0100

    added name methods to pupil and teacher

--- app/models/pupil.rb
+++ app/models/pupil.rb
@@ -3,4 +3,8 @@ class Pupil < ActiveRecord::Base
 	has_many :given_votes, foreign_key: "voter_id", class_name: "Vote"
 	has_many :achieved_votes, class_name: "Vote", :as => :voted
 
+	def name
+		first_name + " " + last_name
+	end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,8 @@ class Teacher < ActiveRecord::Base
 
 	has_many :achieved_votes, class_name: "Vote", :as => :voted
 
+	def name
+		first_name + " " + last_name
+	end
+
 end
