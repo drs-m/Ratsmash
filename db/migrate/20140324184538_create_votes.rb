e2342dc4e29f34ee75510c3f0d73274f0e92b45b
Darius Mewes <dariusmewes@me.com>
2014-03-24 19:45:45 +0100

    added vote model

---
+++ db/migrate/20140324184538_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.string :voteable_type
+      t.integer :voteable_id
+
+      t.timestamps
+    end
+  end
+end
