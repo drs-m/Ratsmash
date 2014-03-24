da2e8a6eff7b8f1f103c026c211fff351a1c8278
Darius Mewes <dariusmewes@me.com>
2014-03-25 00:03:55 +0100

    added relations

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,5 @@
 class Teacher < ActiveRecord::Base
+
+	has_many :achieved_votes, class_name: "Vote", :as => :voted
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,7 @@
 class Vote < ActiveRecord::Base
+
+	belongs_to :voter, class_name: "Pupil"
+	belongs_to :voted, polymorphic: true
+	belongs_to :category
+
 end
