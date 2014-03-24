6122d92a296d0a758dd8a97347be32fe883d56bf
Darius Mewes <dariusmewes@me.com>
2014-03-25 00:45:15 +0100

    added search methods to pupil and teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,19 @@ class Teacher < ActiveRecord::Base
 
 	has_many :achieved_votes, class_name: "Vote", :as => :voted
 
+	def self.search(params)
+		if (params)
+			querystring = params.keys[0].to_s + " LIKE :" + params.keys[0].to_s
+			i = 1
+			while i < params.length
+				querystring += " AND " + params.keys[i].to_s + " LIKE :" + params.keys[i].to_s
+				i += 1
+			end
+			params.each { |key, value| params[key] = "%" + value + "%" }
+			where(querystring, params) # return results
+		end
+	end
+
 	def name
 		first_name + " " + last_name
 	end
