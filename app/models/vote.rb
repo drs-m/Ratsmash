ac080e1888a66d670dd8e358a7b6e1f0c265c453
Darius Mewes <dariusmewes@me.com>
2014-03-30 21:46:15 +0200

    added method to get first 3 votes by voter and category

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,16 @@
+# encoding: utf-8
 class Vote < ActiveRecord::Base
 
 	belongs_to :voter, class_name: "Student"
 	belongs_to :voted, polymorphic: true
 	belongs_to :category
+
+	def self.by_voter_in_category(options = {})
+		if options[:voter] && options[:category]
+			voted = []
+			3.times { |i| voted[i] = options[:voter].given_votes.where(category_id: options[:category].id, rating: 3-i).first }
+			return voted
+		end
+	end
 
 end
