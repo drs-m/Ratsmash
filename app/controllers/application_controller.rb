af66aa3e8e9e3bf6afc7a1418d5ceb9bdc5c4683
Darius Mewes <dariusmewes@me.com>
2014-05-28 21:36:25 +0200

    deactivated check_session for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
-	helper_method :check_session
+	# helper_method :check_session
 
 	private
 		def check_session(admin_permissions_required = false)
