9983dfcdb85f314523bbfc457b550ebbcf30c0a3
Julius Rückin <sgd>
2014-05-29 20:54:34 +0200

    deleted javascript placeholder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,30 +4,3 @@
 
 //= require voting
 //= require script-notifier
-$(document).ready(function(){
-	if($('#password').val() == "Passwort" || $('#password').val() == ""){
-		$('#password').attr('type','text');
-	}
-	$('#email').focus(function(){
-		if($('#email').val() == "Rats-Mailaddresse" || $('#email').val() == ""){
-			$('#email').val('');
-		}
-	});
-	$('#email').blur(function(){
-		if($('#email').val() == "Rats-Mailaddresse" || $('#email').val() == ""){
-			$('#email').val('Rats-Mailaddresse');
-		}
-	});
-	$('#password').focus(function(){
-		if($('#password').val() == "Passwort" || $('#password').val() == ""){
-			$('#password').attr('type','password');
-			$('#password').val('');
-		}
-	});
-	$('#password').blur(function(){
-		if($('#password').val() == "Passwort" || $('#password').val() == ""){
-			$('#password').attr('type','text');
-			$('#password').val('Passwort');
-		}
-	});
-});
