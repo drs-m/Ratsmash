0c5bcb34c754adee6454ff078e3f0239693b4884
Darius Mewes <dariusmewes@me.com>
2014-05-31 20:27:11 +0200

    increased test students to 132

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,13 +45,29 @@ categories_all_female = ["Toll", "Schön", "Schlau", "Nett", "Blabla"]
 categories_all_female.each { |cp_name| Category.create(name: cp_name).apply_to(:all_female) }
 
 
-
+first_names_female = ["Karin", "Jessica", "Krista", "Laura", "Marianne", "Floriane", "Tusnelda", "Kassandra"]
+first_names_male = ["Thomas", "Peter", "Christoph", "Joe", "Sean", "Tobias", "Hendrik", "Carlo"]
+last_names = ["Schmidt", "Meier", "Müller", "Schulz", "Boden", "Bago", "Gutt", "Knöff"]
 
 # Schüler
 students = [["Darius Mewes", true, "darius.mewes@rats-os.de", true, "test123"], ["Max Mustermann", true, "max.mustermann@rats-os.de", false, "hihi"], ["Julius Rückin", true, "julius.rueckin@rats-os.de", true, "fladenbrot"], ["Inactiva Scholtz", false, "bumm@dong.de", false, "zinzon"]]
 students.each { |student| Student.create name: student[0], gender: student[1], mail_address: student[2], admin_permissions: student[3], password: student[4], password_confirmation: student[4] }
 Student.find(4).update closed: true
 
+# more students
+last_names.each do |last_name|
+	first_names_female.each  { |first_name| Student.create name: "#{first_name} #{last_name}", gender: false, mail_address: "#{first_name}.#{last_name}@rats-os.de", password: "pw", password_confirmation: "pw" }
+	first_names_male.each { |first_name| Student.create name: "#{first_name} #{last_name}", gender: true, mail_address: "#{first_name}.#{last_name}@rats-os.de", password: "pw", password_confirmation: "pw" }
+end
+
 # Lehrer
 teachers = [["Lisa Müller", false], ["Hans-Christian Schmidt", true], ["Florian Obstkorb", true], ["Klarissa Kuh", false], ["Megan Schnase", false], ["Klaus-Markus König", true], ["Christina Tosko", false]]
 teachers.each { |teacher| Teacher.create name: teacher[0], gender: teacher[1] }
+
+# Votes für max mustermann
+max = Student.find_by(name: "Max Mustermann")
+Student.all.each do |student|
+	rating = rand(1..3)
+	max.achieved_votes << student.given_votes.build(category_id: 1, rating: rating)
+	puts student.name + " hat eine Stimme für Max abgegeben (Kategorie: Toll, Rating: #{rating})"
+end
