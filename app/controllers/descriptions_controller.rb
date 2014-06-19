2db5e63aa11c71307c8bc383ced495ef56f7e103
Julius Rückin <sgd>
2014-06-19 21:12:33 +0200

    ordered descritions by status

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -2,7 +2,10 @@ class DescriptionsController < ApplicationController
 	before_action -> { check_session redirect: true }
 
 	def index
-		@descriptions_for_me = Description.where(:for_id => @current_user.id).order(:updated_at).reverse
+		@unaccepted_descriptions_for_me = Description.where(:for_id => @current_user.id, :status => 0).order(:updated_at).reverse
+		@rejected_descriptions_for_me = Description.where(:for_id => @current_user.id, :status => -1).order(:updated_at).reverse
+		@allowed_description_for_me = Description.where(:for_id => @current_user.id, :status => 1).first
+		@written_descriptions = Description.where(:from_id => @current_user.id).order(:updated_at).reverse
 	end
 
 	def new 
@@ -16,7 +19,7 @@ class DescriptionsController < ApplicationController
 			if Student.find_by_name params[:name]
 				for_id = Student.find_by_name(params[:name]).id
 				if for_id != from_id
-					Description.create :for_id => for_id, :from_id => from_id, :content => params[:description], :status => -1, :additional_authors => params[:additional_authors], :interests => params[:interests], :hobbies => params[:hobbies]
+					Description.create :for_id => for_id, :from_id => from_id, :content => params[:description], :status => 0, :additional_authors => params[:additional_authors], :interests => params[:interests], :hobbies => params[:hobbies]
 				end
 			end
 		end
