203a5520463907497387b1d0f80604861a760602
Julius Rückin <sgd>
2014-06-29 15:26:38 +0200

    production environment is now able to send mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,21 @@ Ratsmash::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #mail settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.enable_mail_delivery = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "rmash.com",
+    user_name:            "rmashteam@gmail.com",
+    password:             "rmashabi2015",
+    authentication:       "plain",
+    enable_starttls_auto: true  
+  }
+  
 end
