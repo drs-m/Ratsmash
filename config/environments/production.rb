6e41d3af0d83789f61e737fe16feb8dd796167d5
Julius Rückin <sgd>
2014-06-29 15:28:30 +0200

    mail setting bug fixes for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Ratsmash::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #mail settings
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_deliveries = true
