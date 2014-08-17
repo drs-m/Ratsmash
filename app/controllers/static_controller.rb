9ab33f44c4f56a0f13e4e8234b1c7f26ae7b81b9
Darius Mewes <dariusmewes@me.com>
2014-08-17 18:13:46 +0200

    bugfix with update_head_info

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,14 +6,16 @@ class StaticController < ApplicationController
 
     # controller für statische seiten. methoden fehlen meistens da die seiten eben statisch sind und daher nur durch die view funktionieren
 
-    def update_head_information
-        if params[:token] == "t12y8"
-            head_info_path = Rails.root + "head_info.yml"
+    def update_head_info
+        head_info_path = Rails.root + "head_info.yml"
+        if params[:token] == ENV["HEAD_UPDATE_TOKEN"]
             head_info = { sha: params[:sha], user_mail: params[:user_mail] }
             File.open(head_info_path, "w") { |f| f.write head_info.to_yaml }
             render text: "Success: " + head_info.to_s and return
         else
-            render text: YAML.load_file(head_info_path).to_s
+            render text: "no file" and return unless File.exists?(head_info_path)
+            head_info = YAML.load_file(head_info_path)
+            render text: head_info.to_s if head_info.present?
         end
     end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,9 @@ module ApplicationHelper
 
 	def footer_head_info
 		if true and @current_user.admin_permissions
+			return "" unless File.exists?(Rails.root + "head_info_yml")
 			head_info = YAML.load_file(Rails.root + "head_info.yml")
-			return if head_info.blank?
+			return "" if head_info.blank?
 			github_base_url = "http://github.com/Ratsmash-dev/RMash/commit/"
 			html_blueprint = " <span id='git-version'>latest commit: <a href='%git_url%' target='_blank'>%sha%</a> - by: %user%</span>"
 			html_response = html_blueprint.gsub("%git_url%", github_base_url + head_info[:sha])
