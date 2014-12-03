5d70cba95b1af35457efa9798678ce8bfdd7173d
Darius Mewes <dariusmewes@me.com>
2014-12-04 00:39:33 +0100

    bug fix attempt 3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.development?
-    Ratsmash::Application.config.session_store :cookie_store, key: '_Ratsmash_session', domain: :all
-else
-    Ratsmash::Application.config.session_store :cookie_store, key: '_Ratsmash_session', domain: "rmash.de"
-end
+Ratsmash::Application.config.session_store :cookie_store, key: '_Ratsmash_session', domain: :all
