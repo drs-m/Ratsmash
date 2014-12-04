1476b53f88baed7cf5379e0a33e3a32992887654
Darius Mewes <dariusmewes@me.com>
2014-12-04 21:01:22 +0100

    refactored category and student api

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -9,11 +9,8 @@ class StudentsController < ApplicationController
         students = Student.order :id
         students = students.where gender: (params[:gender] == "m") if params[:gender]
 
-        if params[:p]
-            render json: JSON.pretty_generate(JSON.parse(students.to_json.to_s))
-        else
-            render json: students, except: [:created_at, :updated_at], status: :ok
-        end
+        students = JSON.pretty_generate(JSON.parse(students.to_json.to_s)) if params[:p]
+        render json: students, except: [:created_at, :updated_at], status: :ok
     end
 
 end
