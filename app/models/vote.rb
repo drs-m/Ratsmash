caa637b4898e49d9a06cc33a1a1bee75b9cd70eb
Darius <dariusmewes@me.com>
2015-01-09 17:50:48 +0100

    add top points method

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -9,6 +9,18 @@ class Vote < ActiveRecord::Base
 		"#{self.category.name} (#{self.rating.to_s}): #{self.voter.name} -> #{self.voted.name}"
 	end
 
+	def self.top_points(calc_points = false)
+		counts = {}
+
+		Vote.where(category_id: Category.where(group_id: Group.where(student: true, teacher: false).ids).ids).each do |v|
+			identifier = v.voter.name + " => " + v.voted.name
+			counts[identifier] ||= 0
+			counts[identifier] += calc_points ? v.rating : 1
+		end
+
+		counts.sort_by { |k,v| v }.reverse
+	end
+
 	def self.by_voter_in_category(options = {})
 		if options[:voter] && options[:category]
 			voted = []
